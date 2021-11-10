8b22a2782024a24e182eed42b0116ebf87dd4bd3
Nguyên Phát <86005213+phatng45@users.noreply.github.com>
2021-11-10 19:58:06 +0700

    Add files via upload

---
+++ marketServer/src/main/java/com/ecom/marketServer/MarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.marketServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketServerApplication.class, args);
+	}
+
+}
---
+++ marketServer/src/test/java/com/ecom/marketServer/MarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.marketServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
