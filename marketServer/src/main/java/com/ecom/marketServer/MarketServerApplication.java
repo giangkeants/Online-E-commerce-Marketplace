1cd76bfd2af81dc478a596c0800d89771743aa81
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2021-11-10 20:11:34 +0700

    test

--- marketServer/src/main/java/com/ecom/marketServer/MarketServerApplication.java
+++ marketServer/src/main/java/com/ecom/marketServer/MarketServerApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MarketServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarketServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MarketServerApplication.class, args);
+    }
 
+    k;safsafsaas;
 }
